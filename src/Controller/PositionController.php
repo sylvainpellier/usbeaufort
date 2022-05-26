52a6ad285914069ce49cb232b1e07acaab66ffe9
Sylvain Pellier <pellier@gmail.com>
2022-05-26 07:43:04 +0200

    correction phase 8

--- src/Controller/PositionController.php
+++ src/Controller/PositionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\MeetRepository;
 use App\Repository\PositionRepository;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
@@ -15,7 +16,7 @@ class PositionController extends AbstractController
     /**
      * @Route("/position/echange", name="app_position_echange")
      */
-    public function index(Request $request, EntityManagerInterface $entityManager, PositionRepository $positionRepository): Response
+    public function index(Request $request, MeetRepository $meetRepository, EntityManagerInterface $entityManager, PositionRepository $positionRepository): Response
     {
         $id = $request->get("id");
         $from = $request->get("from");
@@ -29,6 +30,42 @@ class PositionController extends AbstractController
             {
                 $oldFrom = $from_posistion->getPouleTo();
                 $oldTo = $to_posistion->getPouleTo();
+
+                $meetsAFrom = $meetRepository->findBy(["PositionA"=>$from_posistion]);
+                $meetsBFrom = $meetRepository->findBy(["PositionB"=>$from_posistion]);
+
+                $meetsATo = $meetRepository->findBy(["PositionA"=>$to_posistion]);
+                $meetsBTo = $meetRepository->findBy(["PositionB"=>$to_posistion]);
+
+
+
+                foreach($meetsAFrom as $m)
+                {
+                    $m->setPositionA($to_posistion);
+                    $entityManager->persist($m);
+                    $entityManager->flush();
+                }
+
+                foreach($meetsBFrom as $m)
+                {
+                    $m->setPositionB($to_posistion);
+                    $entityManager->persist($m);
+                    $entityManager->flush();
+                }
+
+                foreach($meetsATo as $m)
+                {
+                    $m->setPositionA($from_posistion);
+                    $entityManager->persist($m);
+                    $entityManager->flush();
+                }
+
+                foreach($meetsBTo as $m)
+                {
+                    $m->setPositionB($from_posistion);
+                    $entityManager->persist($m);
+                    $entityManager->flush();
+                }
 
 
 
